c2edcb41bca395d205fb7555a2d878b1cdecaec2
Sayef <khan.sayef0004@gamil.com>
2026-04-17 18:53:06 +0000

    defs.h

--- defs.h
+++ defs.h
@@ -1,3 +1,4 @@
+//#include "proc.h"
 struct buf;
 struct context;
 struct file;
@@ -185,6 +186,7 @@ void            switchuvm(struct proc*);
 void            switchkvm(void);
 int             copyout(pde_t*, uint, void*, uint);
 void            clearpteu(pde_t *pgdir, char *uva);
-
+//#include "proc.h"
+//int                 getprocs(struct pstat *table, int max);
 // number of elements in fixed-size array
 #define NELEM(x) (sizeof(x)/sizeof((x)[0]))
